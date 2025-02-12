e474d872c2bf06f3f128313357ea03c51f1fa422
Jeremy-Jacob <91925286+Jeremy-Jacob@users.noreply.github.com>
2025-02-11 23:16:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ function redirectToYes() {
     window.location.href = "yes-page.html";
   }
   
-  // Function to redirect to the "No" page
+  // Function to redirect to the next "No" page
   function redirectToNo() {
-    window.location.href = "no-page.html";
+    const currentPage = window.location.pathname.split('/').pop();
+    switch (currentPage) {
+      case 'index.html':
+        window.location.href = 'no-page.html';
+        break;
+      case 'no-page.html':
+        window.location.href = 'no-page-2.html';
+        break;
+      case 'no-page-2.html':
+        window.location.href = 'no-page-3.html';
+        break;
+      case 'no-page-3.html':
+        window.location.href = 'no-page-final.html';
+        break;
+      default:
+        break;
+    }
   }
