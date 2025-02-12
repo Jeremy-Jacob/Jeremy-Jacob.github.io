e415c21bbb9e6c67c03bba8b2bddc902444cce96
Jeremy-Jacob <91925286+Jeremy-Jacob@users.noreply.github.com>
2025-02-11 22:47:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to redirect to the "Yes" page
+function redirectToYes() {
+    window.location.href = "yes-page.html";
+  }
+  
+  // Function to redirect to the "No" page
+  function redirectToNo() {
+    window.location.href = "no-page.html";
+  }
